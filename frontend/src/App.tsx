f1cb5ad7395322fe3d2c1ce7e4492f01506f1506
haribizohra <zohra.haribi@esprit.tn>
2025-08-05 15:28:29 +0200

    initialize frontend project with Vite, React, and Bootstrap

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
