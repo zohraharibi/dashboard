3de0a3255529391db25cf8a0c43c37bbc3077419
haribizohra <zohra.haribi@esprit.tn>
2025-08-09 22:52:34 +0200

    Update CORS

--- backend/main.py
+++ backend/main.py
@@ -28,12 +28,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173", 
-        "http://localhost:5174",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        "https://dashboard-frontend-n88x.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
